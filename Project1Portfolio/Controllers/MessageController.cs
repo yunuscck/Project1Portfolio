144f6f94fbbcfc6a8b9bce53169ba0278df81050
Emre <emreciicek@gmail.com>
2024-09-22 01:35:14 +0300

    MessageDetails Added

---
+++ Project1Portfolio/Controllers/MessageController.cs
@@ -0,0 +1,40 @@
+﻿using Project1Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1Portfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        MyPortfolio5DbEntities contex = new MyPortfolio5DbEntities();
+        public ActionResult Inbox()
+        {
+            var values = contex.Message.ToList();
+            return View(values);
+        }
+        public ActionResult MessageDetail(int id)
+        {
+            var value = contex.Message.Where(x=>x.MessageId==id).FirstOrDefault();
+            value.IsRead = true;
+            contex.SaveChanges();
+            return View(value);
+        }
+        public ActionResult MessageStatusChangeToTrue(int id)
+        {
+            var value = contex.Message.Where(x => x.MessageId == id).FirstOrDefault();
+            value.IsRead = true;
+            contex.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public ActionResult MessageStatusChangeToFalse(int id)
+        {
+            var value = contex.Message.Where(x => x.MessageId == id).FirstOrDefault();
+            value.IsRead = false;
+            contex.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+    }
+}
---
+++ Project1Portfolio/Controllers/SkillController.cs
@@ -0,0 +1,39 @@
+﻿using Project1Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1Portfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        MyPortfolio5DbEntities contex = new MyPortfolio5DbEntities();
+        public ActionResult SkillList()
+        {
+            var values = contex.Skill.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSkill(Skill skill)
+        {
+            contex.Skill.Add(skill);
+            contex.SaveChanges();//degisiklikleri kaydet veritabanına yansıt
+            return RedirectToAction("SkillList"); // eklmeyi yap ve beni tekrar Yetenek Listesine gonder
+        }
+
+        public ActionResult DeleteSkill(int id)
+        {
+            var value = contex.Skill.Find(id);
+            contex.Skill.Remove(value);
+            contex.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+    }
+}
