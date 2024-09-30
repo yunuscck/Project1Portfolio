ecfadcb7722ca3d7c80a7cf7cefff4a1ba17d78f
Emre <emreciicek@gmail.com>
2024-09-30 19:38:13 +0300

    Update,create,delete Experience and Statistics added

---
+++ Project1Portfolio/Controllers/ExperienceController.cs
@@ -0,0 +1,61 @@
+﻿using Project1Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1Portfolio.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        // GET: Experience
+        MyPortfolio5DbEntities contex = new MyPortfolio5DbEntities();
+        public ActionResult ExperienceList()
+        {
+            var values = contex.Experience.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateExperience()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateExperience(Experience experience)
+        {
+            contex.Experience.Add(experience);
+            contex.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public ActionResult DeleteExperience(int id)
+        {
+            var value = contex.Experience.Find(id);
+            contex.Experience.Remove(value);
+            contex.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateExperience(int id)
+        {
+            var value = contex.Experience.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateExperience(Experience experience)
+        {
+            var value = contex.Experience.Find(experience.ExperienceId);
+            value.Title = experience.Title;
+            value.Description = experience.Description;
+            value.WorkDate = experience.WorkDate;
+            value.CompanyName = experience.CompanyName;
+            contex.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+    }
+}
